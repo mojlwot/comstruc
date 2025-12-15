1bb84add4be710f8b3add063b6a22ff28b4f2471
mojlwot <garfareth@gmail.com>
2025-12-15 10:19:04 +0700

    fixed

--- last1.cpp
+++ last1.cpp
@@ -3,22 +3,28 @@ int main()
 {
 	int score;
 	scanf("%d",&score);
-	if(score < 100 && score > 0){
-		if (score >= 85){
-			printf("A");
+	if(score < 100){
+		if(score > 0){
+			if (score >= 87){
+				printf("A");
+			}
+			else if (score >= 79){
+				printf("B");		
+			}
+			else if (score >= 71){
+				printf("C", 79-score);		
+			}
+			else if (score >= 63){
+				printf("D");		
+			}
+			else if (score < 12) printf("F");
+		}else {
+			printf("error score");
 		}
-		else if (score >= 75){
-			printf("B");		
-		}
-		else if (score >= 68){
-			printf("C", 75-score);		
-		}
-		else if (score >= 55){
-			printf("D");		
-		}
-		else printf("F");
-	}else printf("error score");
+	}
+	else {
+		printf("error score");
+	}
 
-	
 	return 0;
 }
